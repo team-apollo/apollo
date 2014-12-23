e04d7945421b525ef02c471210a02576fb129e7b
Tom Willis <twillis@topgunhq.com>
2014-12-23 16:16:29 -0500

    redirect / -> /index.html

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.3.2"]
-                 [ring/ring-jetty-adapter "1.3.2"]
+                 [info.sunng/ring-jetty9-adapter "0.7.3"]
                  [compojure "1.3.1"]]
   :main cljspazzer.core)
--- src/cljspazzer/core.clj
+++ src/cljspazzer/core.clj
@@ -1,14 +1,17 @@
 (ns cljspazzer.core
-  (:require  [ring.adapter.jetty :as j])
+  (:require  [ring.adapter.jetty9 :as j]
+             [ring.middleware.resource :as m]
+             [compojure.core :refer :all]
+             [compojure.route :as route]
+             [compojure.response :refer [render]]
+             [clojure.java.io :as io])
   (:gen-class :main true))
 
-
-(defn app-handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/plain;=us-ascii"}
-   :body (str request)})
+(defroutes app
+  (GET "/" [] {:status 302
+               :headers {"Location" "/index.html"}}))
 
 (defn -main
   "Launch the web server app"
   [& args]
-  (j/run-jetty app-handler {:port 3000}))
+  (j/run-jetty (m/wrap-resource app "public") {:port 5050}))

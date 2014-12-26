68226b5824a8ff4d695bedf32dca0131aac27953
Tom Willis <twillis@topgunhq.com>
2014-12-26 13:39:30 -0500

    re-factor for readability

--- src/clj/cljspazzer/scanner.clj
+++ src/clj/cljspazzer/scanner.clj
@@ -9,12 +9,18 @@
 (defn is-audio-file? [f]
   (.accept file-filter f))
 
+(defn val-not-null? [kv]
+  (not (= (last kv) "null")))
+
+(defn hm-filter-null [hm]
+  (apply hash-map
+         (flatten
+          (filter val-not-null?
+                  (seq hm)))))
+
 (defn get-info [f]
   (let [id3tags (id3/read-tag f)
-        id3tags-fixed (apply hash-map
-                             (flatten
-                              (filter (fn [x] (not (= (last x) "null")))
-                                      (seq id3tags))))]
+        id3tags-fixed (hm-filter-null id3tags)]
     (assoc id3tags-fixed
            :path (.getAbsolutePath f)
            :last-modified (.lastModified f))))

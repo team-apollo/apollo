43951bf98ee1a0cd96ba6061e36e00bab1da84bb
Tom Willis <twillis@topgunhq.com>
2014-12-29 08:45:56 -0500

    first stab at db update ops, including insert/update/delete/upsert track and prune tracks
    also put unique contraint on path

--- src/clj/cljspazzer/db/schema.clj
+++ src/clj/cljspazzer/db/schema.clj
@@ -1,5 +1,6 @@
 (ns cljspazzer.db.schema
-  (:require [clojure.java.jdbc :as sql]))
+  (:require [clojure.java.jdbc :as sql]
+            [clojure.java.io :as io]))
 
 (def the-db {:classname "org.sqlite.JDBC",
              :subprotocol "sqlite",
@@ -8,7 +9,7 @@
 
 (def tables {:tracks {:name "tracks"
                       :columns[[:id :integer "PRIMARY KEY" "AUTOINCREMENT"]
-                               [:path :string]
+                               [:path :string "UNIQUE"]
                                [:title :string]
                                [:artist :string]
                                [:year :string]
@@ -43,8 +44,29 @@
        (sql/query db ["select count(id) as count from tracks where path=?" t])))
      0))
 
-(defn insert-track! [db track-info]
-  (sql/insert! db :tracks track-info))
+(defn update-track!
+  "returns count of rows affected"
+  [db track-info]
+  (first (sql/update! db :tracks track-info ["path=?" (:path track-info)])))
+
+(defn insert-track!
+  "returns last row id which should correspond to id"
+  [db track-info]
+  ((keyword "last_insert_rowid()") (first (sql/insert! db :tracks track-info))))
+
+(defn delete-track!
+  "returns rows effected which should be > 1 if successful"
+  [db path]
+  (first (sql/delete! db :tracks ["path=?" path])))
+
+(defn upsert-track!
+  "returns {id:??}for intert or {row-count:??} for update"
+  [db track-info]
+  (let [update-result (update-track! db track-info)]
+    (if (> update-result 0)
+      {:row-count update-result :action :update}
+      {:id (insert-track! db track-info) :action :insert})))
+
 
 (defn last-modified-index
   "{path:last_modified}"
@@ -55,3 +77,9 @@
                                            r (hash-map k v)]
                                        r)) rows))]
     result))
+
+(defn prune-tracks
+  "get rid of rows where the files no longer exist"
+  [db]
+  (let [files (filter (fn [f] (not (.exists f))) (map io/file (keys (last-modified-index db))))]
+    (map (partial delete-track! db) (map (fn [f] (.getAbsolutePath f)) files))))

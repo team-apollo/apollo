d55b43e2651b357029143ecac7bc274a6516abce
Tom Willis <twillis@topgunhq.com>
2015-01-06 22:01:30 -0500

    getting a handle on these snippet things

--- src/cljs/cljspazzer/client/pages.cljs
+++ src/cljs/cljspazzer/client/pages.cljs
@@ -8,33 +8,43 @@
 
 (defsnippet artist-item "templates/browser.html" [:.artist-item]
   [artist]
-  {[:a] (do-> (content artist)
+  {
+   [:a] (do-> (content artist)
               (set-attr :href (utils/format "#/artists/%s" (utils/encode artist))))})
 
 (defsnippet index-item "templates/browser.html" [:.artist-index]
   [idx]
-  {[:a] (do-> (content idx)
+  {
+   [:a] (do-> (content idx)
               (set-attr :href (utils/format "#/nav/%s" idx)))})
 (defsnippet track-item "templates/browser.html" [:.track-item]
   [track]
-  {[:.name] (content "goo")}
+  {
+   [:.name] (content track)}
   )
 (defsnippet album-item "templates/browser.html" [:.album-item]
   [album]
-  {[:.album-heading] (content (:name album))
+  {
+   [:.album-heading] (content (:name album))
    [:.track-list] (content (map track-item (:tracks album)))
    })
 
 (defsnippet album-list "templates/browser.html" [:.album-list]
   [artist albums]
-  {[:.album-item] (do-> (content map album-item albums))
-   [:.artist-heading] (content artist)})
+  {
+   [:.album-item] (content (map album-item albums))
+   [:.artist-heading] (content artist)
+   }
+  )
 
 (deftemplate browse-page "templates/browser.html" [data]
-  {[:.artist-list] (content (map artist-item (:artists data)))
+  {
+   [:.artist-list] (content (map artist-item (:artists data)))
    [:.artist-nav] (content (map index-item "abcdefghijklmnopqrstuvwxyz"))
-   [:.album-list] (content (album-list (:active-artist data)
-                                (:albums data)))})
+   [:.album-list] (content (album-list (:active-artist data "converge")
+                                       (:albums data [{:name "foo" :tracks ["track1" "track2"]}
+                                                      ])
+                                       ))})
 
 (defn view-browse [data]
   (om/component (browse-page data)))

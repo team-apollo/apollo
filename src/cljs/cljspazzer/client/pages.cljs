4de8e7a727501eaa366a2be4ff465a8fd052aee0
Tom Willis <twillis@topgunhq.com>
2015-01-08 19:50:52 -0500

    added basic grid structure and cleaned up syntax

--- src/cljs/cljspazzer/client/pages.cljs
+++ src/cljs/cljspazzer/client/pages.cljs
@@ -2,24 +2,28 @@
   (:require [om.core :as om :include-macros true]
             [sablono.core :as html :refer-macros [html]]
             [cljspazzer.client.utils :as utils]))
+
 (def nav-seq (concat (map str "#abcdefghijklmnopqrstuvwxyz") ["all"]))
 
 (defn nav-item [x]
-  (html [:li
-         [:a {:href (utils/format "#/nav/%s" x)} x]]))
+  [:li [:a {:href (utils/format "#/nav/%s" x)} x]])
 
 (defn artist-item [x]
-  (html [:li [:a {:href (utils/format "#/artists/%s" x)} x]]))
+  [:li [:a {:href (utils/format "#/artists/%s" x)} x]])
 
 (defn album-item [x]
-  (html [:li (utils/format "%s - (%s)" (x "album_canonical") (x "year"))]))
+  [:li (utils/format "%s - (%s)" (x "album_canonical") (x "year"))])
 
 (defn browse-page [data]
   (html [:div.browse
-         [:div.collection-nav [:ul (map nav-item nav-seq)]]
-         [:div.artist-list [:ul (map artist-item (:artists data))]]
-         [:div.artist-detail [:ul (map album-item (:albums data))]]]
-        ))
+         [:div.pure-g
+          [:div.collection-nav.pure-u-5-5 [:ul (map nav-item nav-seq)]]]
+         [:div.content.pure-g
+          [:div.artist-list.pure-u-1-5
+           [:ul (map artist-item (:artists data))]]
+          [:div.artist-detail.pure-u-3-5
+           [:h1 (:active-artist data)]
+           [:ul (map album-item (:albums data))]]]]))
 
 (defn view-browse [data]
   (om/component (browse-page data)))

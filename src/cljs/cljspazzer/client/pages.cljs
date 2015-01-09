88300933c41102483141573da8486b88234f2176
Tom Willis <twillis@topgunhq.com>
2015-01-08 20:13:10 -0500

    url encode so that the param makes it to the back end un-molested

--- src/cljs/cljspazzer/client/pages.cljs
+++ src/cljs/cljspazzer/client/pages.cljs
@@ -6,10 +6,10 @@
 (def nav-seq (concat (map str "#abcdefghijklmnopqrstuvwxyz") ["all"]))
 
 (defn nav-item [x]
-  [:li [:a {:href (utils/format "#/nav/%s" x)} x]])
+  [:li [:a {:href (utils/format "#/nav/%s" (utils/encode x))} x]])
 
 (defn artist-item [x]
-  [:li [:a {:href (utils/format "#/artists/%s" x)} x]])
+  [:li [:a {:href (utils/format "#/artists/%s" (utils/encode x))} x]])
 
 (defn album-item [x]
   [:li (utils/format "%s - (%s)" (x "album_canonical") (x "year"))])

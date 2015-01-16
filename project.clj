df7acd262da9f7d12709fd57d22190f594b75fa6
Tom Willis <twillis@topgunhq.com>
2015-01-16 12:38:05 -0500

    lein-sass integrtion

--- project.clj
+++ project.clj
@@ -17,14 +17,15 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [secretary "1.2.1"]
                  [cljs-ajax "0.3.3"]
-                 [om "0.8.0-rc1"]
+                 [org.om/om "0.8.0"]
                  [bk/ring-gzip "0.1.1"]
                  [sablono "0.2.22"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :source-paths ["src/clj"
                  "src/cljs"]
   :plugins [[lein-ring "0.8.13"]
-            [lein-cljsbuild "1.0.4-SNAPSHOT"]]
+            [lein-cljsbuild "1.0.4-SNAPSHOT"]
+            [lein-haml-sass "0.2.7-SNAPSHOT"]]
   :hooks [leiningen.cljsbuild]
   :ring {:handler cljspazzer.core/app
          :port 5050}
@@ -34,6 +35,16 @@
                 :compiler {:output-to "resources/public/javascripts/cljspazzer/main.js"
                            :optimizations :none
                            :cache-analysis true
-                           :pretty-print false
+                           :pretty-print true
+                           :pseudo-names true
                            :source-map true
-                           :output-dir "resources/public/javascripts/cljspazzer/"}}]})
+                           :output-dir "resources/public/javascripts/cljspazzer/"}}]}
+   :sass {:src "resources/sass"
+         :output-directory "resources/public/css"
+         ;; Other options (provided are default values)
+         :output-extension "css"
+         ;; :auto-compile-delay 250
+         ;; :delete-output-dir true ;; -> when running lein clean it will delete the output directory if it does not contain any file
+         ;; :ignore-hooks [:clean :compile :deps] ;; -> if you use the hooks, this option allows you to remove some hooks that you don't want to run
+         ;; :gem-version "3.2.1"
+         })

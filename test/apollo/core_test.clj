0a40c9ac6f4bc530b3520a3541b2b072186598e1
Tom Willis <twillis@topgunhq.com>
2015-03-29 20:44:03 -0400

    actual tests... it's a start

--- test/apollo/core_test.clj
+++ test/apollo/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [apollo.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn request [handler method resource & params]
+  (handler {:uri resource :request-method method :params params}))
+
+(deftest is-dev-true
+  (testing "is dev true"
+    (is (= is-dev? true))))
+
+(deftest is-inject-devcode-working
+  (testing "given is-dev? is true we should see some injecting of figwheel code"
+    (is (boolean (re-find #"('apollo.client.dev')" (:body (render-to-response (page))))))))
+
+(deftest redirects-to-index
+  (testing "redirects from / to index.html"
+    (let [{status :status {location "Location"} :headers} (request app-handler :get "/")]
+      (is (= 302 status))
+      (is (= location "/index.html")))))

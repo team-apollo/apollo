b24090ed5bb1267cbae15d95eba6fd23b6789bd5
Tom Willis <tom.willis@gmail.com>
2015-10-11 09:58:10 -0400

    increase core test coverage(better than nothing)

--- test/apollo/core_test.clj
+++ test/apollo/core_test.clj
@@ -1,6 +1,7 @@
 (ns apollo.core-test
-  (:require [clojure.test :refer :all]
-            [apollo.core :refer :all]))
+  (:require [apollo.core :refer :all]
+            [apollo.db.schema :as s]
+            [clojure.test :refer :all]))
 
 (defn request [handler method resource & params]
   (handler {:uri resource :request-method method :params params}))
@@ -18,3 +19,38 @@
     (let [{status :status {location "Location"} :headers} (request app-handler :get "/")]
       (is (= 302 status))
       (is (= location "/index.html")))))
+
+(deftest get-index
+  (testing "getting index.html"
+    (let [{status :status} (request app-handler :get "/index.html")]
+      (is (= 200 status)))))
+
+(deftest recently-added
+  (testing "get list of recently added"
+    (let [{status :status} (request app-handler :get "/api/recently-added")]
+      (is (= 200 status)))))
+
+(deftest by-year
+  (testing "list by year"
+    (let [{status :status} (request app-handler :get "/api/by-year")]
+         (is (= 200 status)))))
+
+
+(deftest get-mounts
+  (testing "get list of mounts"
+    (let [{status :status} (request app-handler :get "/api/mounts")]
+      (is (= 200 status)))))
+
+
+(deftest artist-api
+  (testing "hit artist api"
+    (let [{artist :artist_id album :id} (first (s/get-albums-recently-added))
+          {id :id} (first (s/tracks-by-album album))]
+      (is (= 200 (:status (request app-handler :get (str "/api/artists/" artist "/info")))))
+      (is (= 404 (:status (request app-handler :get (str "/api/artists/" artist "/image")))))
+      (is (= 200 (:status (request app-handler :get (str "/api/artists/" artist )))))
+      (is (= 200 (:status (request app-handler :get (str "/api/artists/" artist "/albums/" album)))))
+      (is (= 200 (:status (request app-handler :get (str "/api/artists/" artist "/albums/" album "/image")))))
+      (is (= 200 (:status (request app-handler :get (str "/api/artists/" artist "/albums/" album "/zip")))))
+      (is (= 200 (:status (request app-handler :get (str "/api/artists/" artist "/albums/" album "/tracks/" id)))))
+      (is (= 200 (:status (request app-handler :get (str "/api/artists/search/" (str (take 3 artist))))))))))
